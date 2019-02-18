9a88875d7977d4f17cfacc8435b177dd599326aa
Ramy Hakam <pencilsoft1@gmail.com>
2019-02-18 23:06:25 +0200

    Right Single Responsablity Principle

---
+++ Classes/Keeper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramy
+ * Date: 2/18/19
+ * Time: 10:55 PM
+ */
+namespace  Classes;
+
+class Keeper
+{
+
+}
--- Classes/Match.php
+++ Classes/Match.php
@@ -6,19 +6,29 @@
  * Time: 10:51 PM
  */
 
+namespace  Classes;
+
 class Match
 {
 
 
-    public  function  attack(){
+    private  $attack;
+    private  $defense;
+    private  $keeper;
+
+    public function __construct()
+    {
+        $this->attack= new Attack();
+        $this->defense= new Defense();
+        $this->keeper= new Keeper();
 
 
     }
-     public  function  defense(){
 
-     }
-     public  function  keeper(){
+    public  function  start(){
+        echo "start play";
+    }
 
-     }
+
 
 }
